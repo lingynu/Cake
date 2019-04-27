62e27d25bd84a583898ca60f6bebd696dc11b495
lingynu <43696004+lingynu@users.noreply.github.com>
2019-04-27 18:15:50 +0800

    Revert "Pjq"

--- src/edu/hbuas/cake/model/dao/CommentReplyDAO.java
+++
@@ -1,7 +0,0 @@
-package edu.hbuas.cake.model.dao;
-
-import edu.hbuas.cake.model.javabean.CommentReply;
-
-public interface CommentReplyDAO {
-    public boolean addCommentReply(CommentReply commentReply);
-}
